3638bceaaf8ebd98d7f696a2bf47940bbbc016c4
Derek <nightwing917@hotmail.com>
2019-07-31 19:03:32 -0600

    No error in validate name

--- app.js
+++ app.js
@@ -6,3 +6,38 @@ document.getElementById('email').addEventListener('blur', validateEmail);
 document.getElementById('phone').addEventListener('blur', validatePhone);
 
 
+// validate name function
+function validateName() {
+    const name = document.getElementById('name');
+    const re = /^[a-zA-Z]{2,20}$/;
+    
+    if(!re.test(name.value)) {
+      name.classList.add('is-invalid');
+    } else {
+      name.classList.remove('is-invalid');
+    }
+}
+
+
+
+// validate zip function
+function validateZip() {
+    console.log('zip connected!');
+}
+
+
+
+// validate email function
+function validateEmail() {
+    console.log('email connected!');
+}
+
+
+
+// validate phone function
+function validatePhone() {
+    console.log('phone connected!');
+}
+
+
+
