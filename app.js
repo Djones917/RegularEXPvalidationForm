985406607e3a5a7a740788f20bcbee414794cb6d
Derek <nightwing917@hotmail.com>
2019-08-01 19:32:42 -0600

    finished regular expressions validation form

--- app.js
+++ app.js
@@ -50,7 +50,14 @@ function validateEmail() {
 
 // validate phone function
 function validatePhone() {
-    console.log('phone connected!');
+  const phone = document.getElementById('phone');
+  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+
+  if (!re.test(phone.value)) {
+    phone.classList.add('is-invalid');
+  } else {
+    phone.classList.remove('is-invalid');
+  }   
 }
 
 
